fef47cbc8c73a2c1904af200a3cc75d730e73975
Magnumvf <magnumvf@gmail.com>
2024-01-26 21:52:45 -0300

    WIP-Primeira subida

---
+++ DigitalStore/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './Header.module.css'
+import Logo from './Logo.jsx'
+
+const Header = () => {
+  return (
+    <div>
+        <Logo />
+    </div>
+  )
+}
+
+export default Header
---
+++ DigitalStore/src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Group from '../assets/Group.svg'
+
+const logo = () => {
+  return (
+    <div>
+        <img src={Group} alt="Logo Digital College" />
+        <p className='LogoCabecalho'>Digital Store</p>
+            </div>
+  )
+}
+
+export default logo
